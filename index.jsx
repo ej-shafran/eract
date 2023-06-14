2b02d9c7a983ace7847c4b7b7b6e6457f317c5f8
ej-shafran <ejshafran@hilma.tech>
2023-06-14 13:25:18 +0300

    Add util functions

--- index.jsx
+++ index.jsx
@@ -1,4 +1,18 @@
 // Hot Module Reloading
 new EventSource('/esbuild').addEventListener('change', () => location.reload());
 
+function TODO() {
+  throw new Error("Not yet implemented!");
+}
+
+function INVALID(debug) {
+  console.error(
+    `Invalid state reached; should not have gotten here! :(\n\t%c Debug Info: %c ${debug}`,
+    "font-weight:bold;",
+    "font-family:monospace;background-color:#222;padding:1px 4px;border-radius:3px;"
+  );
+
+  throw new Error("INVALID");
+}
+
 console.log("Hello, world!");
