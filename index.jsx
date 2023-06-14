e3ec06c4407ffde53ed176123994f321d3d81467
ej-shafran <ejshafran@hilma.tech>
2023-06-14 13:25:04 +0300

    Some more setup

--- index.jsx
+++ index.jsx
@@ -1,4 +1,31 @@
 // Hot Module Reloading
-new EventSource('/esbuild').addEventListener('change', () => location.reload());
+new EventSource("/esbuild").addEventListener("change", () => location.reload());
 
-console.log("Hello, world!");
+function TODO() {
+  throw new Error("Not yet implemented!");
+}
+
+function INVALID(debug) {
+  console.error(
+    `Invalid state reached; should not have gotten here! :(\n\t%c Debug Info: %c ${debug}`,
+    "font-weight:bold;",
+    "font-family:monospace;background-color:#222;padding:1px 4px;border-radius:3px;"
+  );
+
+  throw new Error("INVALID");
+}
+
+const $$typeof = Symbol.for("eract.element");
+
+const React = {
+  createElement: (type, props, ...children) => {
+    props.children = children.filter(Boolean).map((child) => {
+      if (typeof child === "object" && child.$$typeof === $$typeof) {
+        return child;
+      }
+    });
+    console.log("args: ", args);
+  },
+};
+
+console.log(<div>Hello, world!</div>);
