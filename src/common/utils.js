bc2799aa0303ee7b36b913b113939874c281f39f
ej-shafran <ejshafran@hilma.tech>
2023-06-16 19:47:15 +0300

    Update utils

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,17 +1,39 @@
-export function TODO() {
+const ERROR_INFO_STYLES = "font-weight:bold;";
+const ERROR_MESSAGE_STYLES =
+  "font-family:monospace;background-color:#222;padding:1px 4px;border-radius:3px;";
+
+/**
+ * todo.
+ */
+export function todo() {
   throw new Error("Not yet implemented!");
 }
 
 /**
- * INVALID.
+ * invariant.
+ *
+ * @param {boolean} condition
+ * @param {string | undefined} message
+ */
+export function invariant(condition, message = "reason unknown") {
+  console.error(
+    `Invariance failed; \n%c Reason: %c ${message}`,
+    ERROR_INFO_STYLES,
+    ERROR_MESSAGE_STYLES
+  );
+  if (!condition) throw new Error(`INVARIANCE: ${message}`);
+}
+
+/**
+ * invalid.
  *
  * @param {unknown} debug
  */
-export function INVALID(debug) {
+export function invalid(debug) {
   console.error(
     `Invalid state reached; should not have gotten here! :(\n\t%c Debug Info: %c ${debug}`,
-    "font-weight:bold;",
-    "font-family:monospace;background-color:#222;padding:1px 4px;border-radius:3px;"
+    ERROR_INFO_STYLES,
+    ERROR_MESSAGE_STYLES
   );
 
   throw new Error("INVALID");
