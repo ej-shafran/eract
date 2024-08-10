ad99d43fd6939277d80a9d4c432d52fdf83d4665
ejshafran <ejshafran@hilma.tech>
2024-08-11 00:19:38 +0300

    Debug logging
    Seems like the state *is* changing - what do we need to do?

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ function useState(initialState) {
   let state = initialState;
   function setState(updater) {
     state = typeof updater === "function" ? updater(state) : updater;
+    console.log("state = ", state);
   }
   return [state, setState];
 }
