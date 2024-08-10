70fbdc0836770eed44fb06c640c185346a6b97d1
ejshafran <ejshafran@hilma.tech>
2024-08-10 23:52:16 +0300

    Add `ReactDOM.render`

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,15 @@ function createElement(type, props = {}, ...children) {
 
 const React = { createElement };
 
-console.log(<div>Hello, world!</div>);
+function render(element, domNode) {
+  todo();
+}
+
+const ReactDOM = { render };
+
+const element = (<div>Hello, world!</div>);
+const domNode = document.getElementById("root");
+ReactDOM.render(element, domNode);
 
 // HOT MODULE RELOADING - makes sure the page refreshes when we save the files
 import "./common/hmr";
