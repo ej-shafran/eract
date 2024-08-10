08af6f89f0f37a19c482426aaac2e03aebe58432
ejshafran <ejshafran@hilma.tech>
2024-08-11 00:11:40 +0300

    Add function prop

--- src/index.jsx
+++ src/index.jsx
@@ -55,7 +55,7 @@ function render(element, domNode) {
 
 const ReactDOM = { render };
 
-const element = (<div id="app">Hello, world!</div>);
+const element = (<div id="app" onClick={() => console.log("hi!")}>Hello, world!</div>);
 const domNode = document.getElementById("root");
 ReactDOM.render(element, domNode);
 
