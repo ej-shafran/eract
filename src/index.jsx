20ce4299d3a58b20a4954027df6f809d6dc8811d
ejshafran <ejshafran@hilma.tech>
2024-08-10 23:50:23 +0300

    Add `React.createElement` as todo

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
-const React = {};
+import { todo } from "./common/utils";
+
+function createElement() {
+  todo();
+}
+
+const React = { createElement };
 
 console.log(<div>Hello, world!</div>);
 
